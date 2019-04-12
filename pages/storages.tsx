022a7fd40a3dab3ae624031a974701e8790dce26
Khoa Pham <kenjifam@gmail.com>
2019-04-12 16:03:24 +0300

    routing

---
+++ pages/storages.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Storages = () => (
+  <>
+    <Head>
+      <title>UpCloud</title>
+    </Head>
+    <main>
+      Welcome to Storages
+    </main>
+  </>
+);
+
+export default Storages;
