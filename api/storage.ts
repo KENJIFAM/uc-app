f6792655503182617ea0ede082ce33ff59607b03
Khoa Pham <kenjifam@gmail.com>
2019-04-12 16:43:42 +0300

    getStorages api

--- api/storage.ts
+++ api/storage.ts
@@ -1,3 +1,16 @@
 //
 // TODO: add getStorages()
 //
+
+// import { request } from 'api/apiClient';
+import data from './data';
+
+async function getStorages() {
+  // const response = await request({ method: 'get', url: '/storage' });
+  // const storages = response.data.storages.storage;
+  // return { data: storages, error: null, status: response.status };
+  const storages = data.storages.storage;
+  return { data: storages, error: null, status: 'ok' };
+}
+
+export { getStorages };
